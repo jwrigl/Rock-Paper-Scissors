77a39d2e3a2e9894c034c943c67010c63ddd57ee
jwrigl <124375480+jwrigl@users.noreply.github.com>
2023-04-11 15:50:08 +0100

    Add outline of showdown function

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 function getComputerChoice(){
     const choices=["Rock","Paper","Scissors"]
     computerChoice = choices[Math.floor(Math.random()*3)]
-    return computerChoice
+    return computerChoice;
+}
+
+function showdown(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log("It's a draw!")
+    }
+    else {
+        if (playerSelection === "Rock") {
+            if (computerSelection === "Paper") {
+                console.log("Computer wins!")
+            }
+            else {
+                console.log("Player wins!")
+            }
+        
+
+        }
+        if (playerSelection === "Paper") {
+            if (computerSelection === "Scissors") {
+                console.log("Computer wins!")
+            }
+            else {
+                console.log("Player wins!")
+            }
+        }
+        if (playerSelection === "Scissors") {
+            if (computerSelection === "Rock") {
+                console.log("Computer wins!")
+            }
+            else {
+                console.log("Player wins!")
+            }
+        }
+    }
 }
