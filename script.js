581d1568e6cdd9f67af72c13ffeb2625c1a061a5
jwrigl <124375480+jwrigl@users.noreply.github.com>
2023-04-11 17:14:18 +0100

    Add game function. Add functionality to decide the winner.

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ function getComputerChoice(){
 function showdown(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === computerSelection) {
         console.log("It's a draw!");
-        let winner = "none"
+        let winner = "none";
     }
     else {
         if (playerSelection.toLowerCase() === "rock") {
             if (computerSelection === "paper") {
                 console.log(`You Lose! ${computerSelection} beats ${playerSelection}.`);
-                let winner = "computer"
+                let winner = "computer";
             }
             else {
                 console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
-                let winner = "human"
+                let winner = "human";
             }
         
 
@@ -25,26 +25,54 @@ function showdown(playerSelection, computerSelection) {
         else if (playerSelection.toLowerCase() === "paper") {
             if (computerSelection === "scissors") {
                 console.log(`You Lose! ${computerSelection} beats ${playerSelection}.`);
-                let winner = "computer"
+                let winner = "computer";
             }
             else {
                 console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
-                let winner = "human"
+                let winner = "human";
             }
         }
         else if (playerSelection.toLowerCase() === "scissors") {
             if (computerSelection === "rock") {
                 console.log(`You Lose! ${computerSelection} beats ${playerSelection}.`);
-                winner = "computer"
+                winner = "computer";
             }
             else {
                 console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
-                let winner = "human"
+                let winner = "human";
             }
         }
     }
+    return winner
+
+function game(){
+    let humanWins = 0;
+    let computerWins = 0;
+    for (let i = 1; i <= ; i++) {
+        let computerSelection = getComputerChoice();
+        let playerSelection = prompt("Rock, paper, or scissors?");
+        winner = showdown(playerSelection, computerSelection)
+        if (winner === "human") {
+            humanWins++1;
+        }
+        else {
+            computerWins++1;
+        }
+
+    }
+    if (humanWins > computerWins) {
+        console.log(`You are the champion! The score was ${humanWins} to ${computerWins}. Not so clever now, are they?`)
+    }
+    else if (computerWins > humanWins) {
+        console.log(`You are the champion! The score was ${computerWins} to ${humanWins}. The future looks bleak for humanity.`)
+    }
+    else {
+        let champion = "No one";
+        console.log(`No one was the champion! The score was 0 to 0. The future is undecided.`)
+
 }
 
-const computerSelection = getComputerChoice()
-const playerSelection = prompt("Rock, paper, or scissors?")
-showdown(playerSelection, computerSelection)
+
+
+
+}
